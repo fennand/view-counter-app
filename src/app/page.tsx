9eb18e883691f4e6789dda844c34cc666d9f104f
Andrew Fennell <afennell@hotmail.co.uk>
2023-11-27 16:14:07 +0000

    Add time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home() {
+  let time = await fetch("http://worldtimeapi.org/api/timezone/Europe/London", {
+    next: { revalidate: 10 },
+  });
+  const data = await time.json();
+
+  const datetime = new Date(data.datetime);
+  const evenEasierToReadDate = datetime.toLocaleTimeString("en-GB");
+  console.log(data);
+
+  await sql`INSERT INTO ViewsTable (views) SELECT 0 WHERE NOT EXISTS (SELECT * FROM ViewsTable)`;
+  await sql`UPDATE ViewsTable SET views = views + 1`;
+  const result = await sql`SELECT Views FROM ViewsTable`;
+
+  console.log(result);
+
   return (
     <main className="flex min-h-screen flex-col items-center">
       <p>You&apos;re looking at my root route. Roude!</p>
+      <p>This has been viewed {result.rows[0].views}</p>
+      <p>The time is : {evenEasierToReadDate}</p>
     </main>
   );
 }
